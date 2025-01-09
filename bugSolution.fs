4aba4c41af7966aeffa638d6b9362f24d60e7456
BugHunter-X <askzenai@gmail.com>
2025-01-09 11:36:06 +0100

    Add bugSolution.fs file

---
+++ bugSolution.fs
@@ -0,0 +1,16 @@
+let add x y = 
+    x + y
+
+let x = 10
+let y = 20
+
+let z = add x y
+
+printf "%d" z //this will print 30
+
+let x2 = 100
+let y2 = 200
+
+let z2 = add x2 y2
+
+printf "%d" z2 //this will print 300
